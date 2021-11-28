387db022f3a8084d1a7a018757807709027df096
Mark Krautheim <mark.krautheim@gmail.com>
2021-11-28 16:25:09 -0500

    Initial commit

---
+++ src/update_stocks_spreadsheet.py
@@ -0,0 +1,56 @@
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import yfinance as yf
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+# My test spreadsheet values (workbook 'MSKQuickstartTest')
+SPREADSHEET_ID = '16T10gT1AIUgbQl6kHE81rEQ9RChdcaLLk1YCpZ1dQIA'
+READ_RANGE_NAME = 'Sheet1!A2:B'
+
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+values = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                            range=READ_RANGE_NAME).execute()['values']
+
+# Pull out relevant data
+tickers = [e[0] for e in values]
+numshares = [int(e[1]) for e in values]
+
+sts = yf.Tickers(tickers)
+sdata = sts.download()
+
+stkprices = [sdata['Close'][tkr].dropna()[-1] for tkr in tickers]
+stkvalues = [[sp, num * sp] for sp, num in zip(stkprices, numshares)]
+
+body = {'values': stkvalues}
+
+sheet.values().update(spreadsheetId=SPREADSHEET_ID, range='C2',
+                      body=body, valueInputOption='RAW').execute()
